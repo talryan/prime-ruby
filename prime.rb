663f3dc74d3060577e3659784574ea538cdf3df0
talryan <nataliamryan@gmail.com>
2020-10-26 17:30:23 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,3 +1,13 @@
 # Add  code here!
-def prime?
+def prime? (number)
+return false if !number.integer?  
+    
+    #less than 2? Should return false
+    return false if number < 2
+    #Whole number? Should return false if a decimal
+    return true if number == 2
+    #Should return true if the number is 2 
+    (2..number-1).each {|num| return false if number % num == 0}
+true 
+
 end 
